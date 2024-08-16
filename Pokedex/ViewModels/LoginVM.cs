31130dd9595c6252d6d64c8ebb4e8c90763adb9c
RenanZenaro <renanzenaro@hotmail.com>
2024-08-16 20:39:49 -0300

    Criação da área administrativa e login

---
+++ Pokedex/ViewModels/LoginVM.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Pokedex.ViewModels;
+
+public class LoginVM
+{
+    [Display(Name = "Email ou Nome de Usuário", Prompt = "Email ou Nome de Usuário")]
+    [Required(ErrorMessage = "Informe o seu email ou nome de usuário")]
+    public string Email { get; set; }
+    [Display(Name = "Senha de Acesso", Prompt = "Senha de Acesso")]
+    [Required(ErrorMessage = "Informe o sua senha de acesso")]
+    public string Senha { get; set; }
+    [Display(Name = "Manter Conectado?")]
+    public bool Lembrar { get; set; } = false;
+    public string UrlRetorno { get; set; }
+}
